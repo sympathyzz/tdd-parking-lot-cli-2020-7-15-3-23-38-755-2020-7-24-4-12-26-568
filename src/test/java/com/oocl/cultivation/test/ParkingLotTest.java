c66af2af33c6853163ae29928e0c9fa1711f7972
GAOGA4 <961977096@qq.com>
2020-07-24 16:04:40 +0800

    [Gavin] test:add and implement should_return_correspond_car_when_fetch_given_correspond_ticket test

--- src/test/java/com/oocl/cultivation/test/ParkingLotTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingLotTest.java
@@ -5,6 +5,7 @@ import com.oocl.cultivation.ParkingLot;
 import com.oocl.cultivation.Ticket;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ParkingLotTest {
@@ -29,5 +30,20 @@ public class ParkingLotTest {
         Car fetchedCar=parkingLot.fetch(ticket);
         //then
         assertNotNull(fetchedCar);
+    }
+
+    @Test
+    void should_return_correspond_car_when_fetch_given_correspond_ticket() {
+        //given
+        Car car=new Car();
+        ParkingLot parkingLot=new ParkingLot();
+        Ticket ticket1=parkingLot.park(car);
+        Car car2=new Car();
+        Ticket ticket2=parkingLot.park(car2);
+        //when
+        Car fetchedCar1=parkingLot.fetch(ticket1);
+        Car fetchedCar2=parkingLot.fetch(ticket2);
+        //then
+        assertNotEquals(fetchedCar1,fetchedCar2);
     }
 }
