dbee670cb531c0ee4d933107f6b3022f1496d0ab
96197 <961977096@qq.com>
2020-07-26 01:15:00 +0800

    [Story4][Gavin] test:implement should_first_parkinglot_has_1_car__second_parkinglot_has_6_cars_when_park_given_7_cars test

--- src/main/java/com/oocl/cultivation/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -1,16 +1,30 @@
 package com.oocl.cultivation;
 
-public class SmartParkingBoy extends ParkingBoy{
+import java.util.*;
 
+public class SmartParkingBoy extends ParkingBoy{
+    private Map<Integer,Integer> parkingLotMap=new HashMap<>();
     public Ticket park(Car car, Customer customer) {
         for(int i=0;i<allParkingLot.size();i++){
-            if(allParkingLot.get(i).getParkingRooms().size()<allParkingLot.get(i).getCapacity()){
-                Ticket ticket = new Ticket(car);
-                allParkingLot.get(i).getParkingRooms().put(ticket, car);
-                car.setIsParking(true);
-                customer.setTicket(ticket);
-                return ticket;
+            int freeParkingLotNumber=allParkingLot.get(i).getCapacity()-allParkingLot.get(i).getParkingRooms().size();
+            parkingLotMap.put(i,freeParkingLotNumber);
+        }
+        Collection<Integer> parkingLotMapValueCollection = parkingLotMap.values();
+        List<Integer> freeParkingLotNumberList = new ArrayList<>(parkingLotMapValueCollection);
+        int maxFreeParkingLotNumber=freeParkingLotNumberList.get(0);
+        int maxFreeParkingLotNumberKey=0;
+        for (int i=0;i<freeParkingLotNumberList.size();i++){
+            if (maxFreeParkingLotNumber<freeParkingLotNumberList.get(i)){
+                maxFreeParkingLotNumber=freeParkingLotNumberList.get(i);
+                maxFreeParkingLotNumberKey=i;
             }
+        }
+        if(maxFreeParkingLotNumber!=0){
+            Ticket ticket = new Ticket(car);
+            allParkingLot.get(maxFreeParkingLotNumberKey).getParkingRooms().put(ticket, car);
+            car.setIsParking(true);
+            customer.setTicket(ticket);
+            return ticket;
         }
         sendMessage(null,customer);
         return null;
