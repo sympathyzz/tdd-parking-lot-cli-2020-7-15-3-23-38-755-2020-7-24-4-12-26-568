c477b61e35bc6dd7514e01612669f988905f4443
96197 <961977096@qq.com>
2020-07-25 18:46:39 +0800

    [Story2][Gavin] test:implement should_get_please_provide_ticket_message_when_fetch_given_no_ticket_by_customer test

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -4,23 +4,34 @@ public class ParkingBoy {
 
     private ParkingLot parkingLot=new ParkingLot();
 
-    public Ticket park(Car car) {
+    public Ticket park(Car car,Customer customer) {
         if(parkingLot.getParkingRooms().size()<parkingLot.getCapacity()){
             Ticket ticket = new Ticket(car);
             parkingLot.getParkingRooms().put(ticket, car);
+            customer.setTicket(ticket);
             return ticket;
         }
         return null;
     }
 
-    public Car fetch(Ticket ticket) {
+    public Car fetch(Ticket ticket,Customer customer) {
         Car car=null;
         if(parkingLot.getParkingRooms().containsKey(ticket)){
             car=parkingLot.getParkingRooms().remove(ticket);
+            customer.setTicket(null);
         }
-        if(car==null){
-        System.out.print("Unrecognized parking ticket.\n");
-        }
+        sendMessage(ticket,customer);
         return car;
+    }
+
+    private void sendMessage(Ticket ticket,Customer customer) {
+        if(customer.getTicket()!=null){
+            if(ticket==null){
+                System.out.print("Please provide your parking ticket.\n");
+            }
+        }else {
+            System.out.print("Unrecognized parking ticket.\n");
+        }
+
     }
 }
--- src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
@@ -27,9 +27,10 @@ public class ParkingBoyTest {
     void should_return_ticket_when_parkCar_given_car() {
         //given
         Car car=new Car();
+        Customer customer=new Customer(car);
         //when
         ParkingBoy parkingBoy=new ParkingBoy();
-        Ticket ticket=parkingBoy.park(car);
+        Ticket ticket=parkingBoy.park(car,customer);
         //then
         assertNotNull(ticket);
     }
@@ -38,10 +39,11 @@ public class ParkingBoyTest {
     void should_return_car_when_fetch_given_parking_ticket() {
         //given
         Car car=new Car();
+        Customer customer=new Customer(car);
         ParkingBoy parkingBoy=new ParkingBoy();
-        Ticket ticket=parkingBoy.park(car);
+        Ticket ticket=parkingBoy.park(car,customer);
         //when
-        Car fetchedCarr=parkingBoy.fetch(ticket);
+        Car fetchedCarr=parkingBoy.fetch(ticket,customer);
         //then
         assertNotNull(fetchedCarr);
     }
@@ -50,13 +52,15 @@ public class ParkingBoyTest {
     void should_return_correspond_car_when_fetch_given_correspond_ticket() {
         //given
         Car car1 = new Car();
+        Customer customer1=new Customer(car1);
         ParkingBoy parkingBoy = new ParkingBoy();
-        Ticket ticket1 = parkingBoy.park(car1);
+        Ticket ticket1 = parkingBoy.park(car1,customer1);
         Car car2 = new Car();
-        Ticket ticket2 = parkingBoy.park(car2);
+        Customer customer2=new Customer(car2);
+        Ticket ticket2 = parkingBoy.park(car2,customer2);
         //when
-        Car fetchedCar1 = parkingBoy.fetch(ticket1);
-        Car fetchedCar2 = parkingBoy.fetch(ticket2);
+        Car fetchedCar1 = parkingBoy.fetch(ticket1,customer1);
+        Car fetchedCar2 = parkingBoy.fetch(ticket2,customer2);
         //then
         assertEquals(car1, fetchedCar1);
         assertEquals(car2, fetchedCar2);
@@ -66,10 +70,11 @@ public class ParkingBoyTest {
     @Test
     void should_return_null_when_fetch_given_wrong_ticket() {
         //given
+        Customer customer=new Customer(new Car());
         Ticket ticket=new Ticket(null);
         ParkingBoy parkingBoy=new ParkingBoy();
         //when
-        Car fetchedCar=parkingBoy.fetch(ticket);
+        Car fetchedCar=parkingBoy.fetch(ticket,customer);
         //then
         assertNull(fetchedCar);
     }
@@ -78,11 +83,12 @@ public class ParkingBoyTest {
     void should_return_null_when_fetch_given_used_ticket() {
         //given
         Car car=new Car();
+        Customer customer=new Customer(car);
         ParkingBoy parkingBoy=new ParkingBoy();
-        Ticket ticket=parkingBoy.park(car);
-        parkingBoy.fetch(ticket);
+        Ticket ticket=parkingBoy.park(car,customer);
+        parkingBoy.fetch(ticket,customer);
         //when
-        Car fetchecCar=parkingBoy.fetch(ticket);
+        Car fetchecCar=parkingBoy.fetch(ticket,customer);
         //then
         assertNull(fetchecCar);
     }
@@ -92,10 +98,13 @@ public class ParkingBoyTest {
         //given
         ParkingBoy parkingBoy=new ParkingBoy();
         for(int i=0;i<10;i++){
-            parkingBoy.park(new Car());
+            Car car=new Car();
+            parkingBoy.park(car,new Customer(car));
         }
         //when
-        Ticket ticket=parkingBoy.park(new Car());
+        Car car=new Car();
+        Customer customer=new Customer(car);
+        Ticket ticket=parkingBoy.park(car,customer);
         //then
         assertNull(ticket);
     }
@@ -104,12 +113,13 @@ public class ParkingBoyTest {
     void should_get_unrecognized_message_when_fetch_given_no_ticket_from_parking_boy() {
         //given
         Car car=new Car();
+        Customer customer=new Customer(car);
         ParkingBoy mockedfetch= Mockito.mock(ParkingBoy.class);
-        given(mockedfetch.park(car)).willReturn(null);
-        Ticket ticket=mockedfetch.park(car);
+        given(mockedfetch.park(car,customer)).willReturn(null);
+        Ticket ticket=mockedfetch.park(car,customer);
         ParkingBoy parkingBoy=new ParkingBoy();
         //when
-        parkingBoy.fetch(ticket);
+        parkingBoy.fetch(ticket,customer);
         //then
         assertThat(systemOut().endsWith("Unrecognized parking ticket.\n")).isTrue();
     }
@@ -118,11 +128,12 @@ public class ParkingBoyTest {
     void should_get_unrecognized_message_when_fetch_given_used_ticket() {
         //given
         Car car=new Car();
+        Customer customer=new Customer(car);
         ParkingBoy parkingBoy=new ParkingBoy();
-        Ticket ticket=parkingBoy.park(car);;
-        parkingBoy.fetch(ticket);
+        Ticket ticket=parkingBoy.park(car,customer);;
+        parkingBoy.fetch(ticket,customer);
         //when
-        parkingBoy.fetch(ticket);
+        parkingBoy.fetch(ticket,customer);
         //then
         assertThat(systemOut().endsWith("Unrecognized parking ticket.\n")).isTrue();
     }
@@ -131,11 +142,12 @@ public class ParkingBoyTest {
     void should_get_please_provide_ticket_message_when_fetch_given_no_ticket_by_customer() {
         //given
         Car car=new Car();
+        Customer customer=new Customer(car);
         ParkingBoy parkingBoy=new ParkingBoy();
-        parkingBoy.park(car);
+        parkingBoy.park(car,customer);
         Ticket ticket=null;
         //when
-        parkingBoy.fetch(ticket);
+        parkingBoy.fetch(ticket,customer);
         //then
         assertThat(systemOut().endsWith("Please provide your parking ticket.\n")).isTrue();
     }
