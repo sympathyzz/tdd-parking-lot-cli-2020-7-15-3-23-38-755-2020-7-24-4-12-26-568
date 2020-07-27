ea2f14fc63f0485125139350eb55aec50f63ec48
96197 <961977096@qq.com>
2020-07-27 19:31:45 +0800

    [Story6][Gavin] test:add management_list_size_is_2_when_add_parking_boy_given_2_parking_boy test

---
+++ src/main/java/com/oocl/cultivation/ParkingLotManager.java
@@ -0,0 +1,5 @@
+package com.oocl.cultivation;
+
+public class ParkingLotManager {
+
+}
---
+++ src/test/java/com/oocl/cultivation/test/ParkingLotManagerTest.java
@@ -0,0 +1,20 @@
+package com.oocl.cultivation.test;
+
+import com.oocl.cultivation.ParkingBoy;
+import com.oocl.cultivation.ParkingLotManager;
+import org.junit.jupiter.api.Test;
+
+public class ParkingLotManagerTest {
+    @Test
+    void management_list_size_is_2_when_add_parking_boy_given_2_parking_boy(){
+        //given
+        ParkingBoy parkingBoy1 = new ParkingBoy();
+        ParkingBoy parkingBoy2 = new ParkingBoy();
+        ParkingLotManager parkingLotManager = new ParkingLotManager();
+        //when
+        parkingLotManager.addParkingBoyToManagementList(parkingBoy1);
+        parkingLotManager.addParkingBoyToManagementList(parkingBoy2);
+        //then
+        assertEquals(2,parkingLotManager.getManagementList().size());
+    }
+}
