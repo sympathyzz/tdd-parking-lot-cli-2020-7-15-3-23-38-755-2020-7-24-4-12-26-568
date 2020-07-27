ee063fae6cca4bec07b69ba91f382115949a1430
96197 <961977096@qq.com>
2020-07-27 19:44:11 +0800

    [Story6][Gavin] update:update management_list_size_is_2_when_add_parking_boy_given_2_parking_boy test

--- src/main/java/com/oocl/cultivation/ParkingLotManager.java
+++ src/main/java/com/oocl/cultivation/ParkingLotManager.java
@@ -4,9 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ParkingLotManager {
+    private ParkingLot parkingLot;
     private List<ParkingBoy> managementList=new ArrayList<>();
     public void addParkingBoyToManagementList(ParkingBoy parkingBoy) {
         managementList.add(parkingBoy);
+    }
+
+    public ParkingLot getParkingLot() {
+        return parkingLot;
     }
 
     public List<ParkingBoy> getManagementList() {
