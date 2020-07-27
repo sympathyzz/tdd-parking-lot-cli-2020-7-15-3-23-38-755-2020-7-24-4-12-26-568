d2041d657e9ddd7c7feb70e73e47ce6dd49d2828
96197 <961977096@qq.com>
2020-07-27 23:03:48 +0800

    [Story6][Gavin] test:add and implement should_get_not_enough_position_message__when_specify_parking_boy_fetch_given_16_cars test

--- src/test/java/com/oocl/cultivation/test/ParkingLotManagerTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingLotManagerTest.java
@@ -1,15 +1,31 @@
 package com.oocl.cultivation.test;
 
 import com.oocl.cultivation.*;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.fest.assertions.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class ParkingLotManagerTest {
+
+    private static ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @BeforeAll
+    public static void setup() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    private String systemOut() {
+        return outContent.toString();
+    }
+
     @Test
     void management_list_size_is_2_when_add_parking_boy_given_2_parking_boy(){
         //given
@@ -72,6 +88,28 @@ public class ParkingLotManagerTest {
         Ticket ticket=parkingLotManager.park(car,customer);
         //then
         assertNotNull(ticket);
+    }
+
+    @Test
+    void should_get_not_enough_position_message__when_specify_parking_boy_fetch_given_16_cars (){
+        //given
+        ParkingBoy parkingBoy=new ParkingBoy();
+        List<ParkingLot> parkingLots=new ArrayList<>();
+        ParkingLot parkingLot=new ParkingLot(5);
+        parkingLots.add(parkingLot);
+        for(int i=0;i<15;i++){
+            Car car=new Car();
+            Customer customer=new Customer(car);
+            ParkingLotManager parkingLotManager = new ParkingLotManager(parkingLots);
+            parkingLotManager.specifyParkingBoyToPark(parkingBoy,car,customer);
+        }
+        //when
+        Car car=new Car();
+        Customer customer=new Customer(car);
+        ParkingLotManager parkingLotManager = new ParkingLotManager(parkingLots);
+        parkingLotManager.specifyParkingBoyToPark(parkingBoy,car,customer);
+        //then
+        assertThat(systemOut().endsWith("Not enough position.\n")).isTrue();
     }
 
 }
